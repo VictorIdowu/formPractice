fcff318e45f18bb202f3b6f1b7f4a8f699bed0cb
VictorIdowu <idsonvictor@gmail.com>
2024-01-11 17:26:53 +0100

    Switching to Tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
-import logoImg from '../assets/logo.jpg';
+import logoImg from "../assets/logo.jpg";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logoImg} alt="A form and a pencil" />
-      <h1>React Forms</h1>
+    <header className="items-center my-8 flex flex-col">
+      <img
+        className="w-20 h-20 object-contain rounded-[50%] border-2 drop-shadow-md"
+        src={logoImg}
+        alt="A form and a pencil"
+      />
+      <h1 className="font-Quicksand font-bold text-5xl my-4 text-[#2f4444]">
+        React Forms
+      </h1>
     </header>
   );
 }
