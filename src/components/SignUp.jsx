b0203bcbbaa1adc873d5301b8cb658ceb230734f
VictorIdowu <idsonvictor@gmail.com>
2024-01-11 19:11:11 +0100

    Switched to SignUp form

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,244 @@
+import { useState } from "react";
+
+export default function Signup() {
+  return (
+    <form className="w-[90%] mx-auto max-w-[40rem] my-12 p-8 bg-gradient-to-b from-[#253c3c] to-[#1d4949] rounded-lg shadow-lg">
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className="mb-4">
+        <label
+          className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          className="block w-full max-w-[15rem] p-2 text-base border border-solid border-[#758a8a] bg-[#869999] text-[#142020]"
+          id="email"
+          type="email"
+          name="email"
+        />
+      </div>
+
+      <div className="flex flex-wrap justify-start gap-4">
+        <div className="mb-4">
+          <label
+            className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="block w-full max-w-[15rem] p-2 text-base border border-solid border-[#758a8a] bg-[#869999] text-[#142020]"
+            id="password"
+            type="password"
+            name="password"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+            htmlFor="confirm-password"
+          >
+            Confirm Password
+          </label>
+          <input
+            className="block w-full max-w-[15rem] p-2 text-base border border-solid border-[#758a8a] bg-[#869999] text-[#142020]"
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+          />
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="flex flex-wrap justify-start gap-4">
+        <div className="mb-4">
+          <label
+            className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+            htmlFor="first-name"
+          >
+            First Name
+          </label>
+          <input
+            className="block w-full max-w-[15rem] p-2 text-base border border-solid border-[#758a8a] bg-[#869999] text-[#142020]"
+            type="text"
+            id="first-name"
+            name="first-name"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+            htmlFor="last-name"
+          >
+            Last Name
+          </label>
+          <input
+            className="block w-full max-w-[15rem] p-2 text-base border border-solid border-[#758a8a] bg-[#869999] text-[#142020]"
+            type="text"
+            id="last-name"
+            name="last-name"
+          />
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <label
+          className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+          htmlFor="phone"
+        >
+          What best describes your role?
+        </label>
+        <select id="role" name="role">
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="mb-4">
+          <input
+            className="block w-full max-w-[15rem] p-2 text-base border border-solid border-[#758a8a] bg-[#869999] text-[#142020]"
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+          />
+          <label
+            className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+            htmlFor="google"
+          >
+            Google
+          </label>
+        </div>
+
+        <div className="mb-4">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+          />
+          <label
+            className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+            htmlFor="friend"
+          >
+            Referred by friend
+          </label>
+        </div>
+
+        <div className="mb-4">
+          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <label
+            className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+            htmlFor="other"
+          >
+            Other
+          </label>
+        </div>
+      </fieldset>
+
+      <div className="mb-4">
+        <label
+          className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+          htmlFor="terms-and-conditions"
+        >
+          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          agree to the terms and conditions
+        </label>
+      </div>
+
+      <p className="flex justify-end gap-4">
+        <button
+          type="reset"
+          className="py-2 px-4 text-base rounded border-none  text-[#9cbaba] bg-transparent focus:text-[#869999] hover:text-[#869999] cursor-pointer"
+        >
+          Reset
+        </button>
+        <button
+          className="text-[#d9e2f1] py-2 px-4 text-base rounded border-none bg-[#147b73] focus:bg-[#319890] hover:bg-[#319890] cursor-pointer"
+          type="submit"
+        >
+          Sign up
+        </button>
+      </p>
+    </form>
+  );
+}
+
+// export function Login() {
+//   const [enteredValue, setEnteredValue] = useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log("Submitted ", enteredValue.email, enteredValue.password);
+//     setEnteredValue({
+//       email: "",
+//       password: "",
+//     });
+//   };
+
+//   const handleInputChange = (identifier, e) => {
+//     setEnteredValue((prev) => ({
+//       ...prev,
+//       [identifier]: e.target.value,
+//     }));
+//   };
+
+//   return (
+//     <form
+//       onSubmit={handleSubmit}
+//       className="w-[90%] mx-auto max-w-[40rem] my-12 p-8 bg-gradient-to-b from-[#253c3c] to-[#1d4949] rounded-lg shadow-lg"
+//     >
+//       <h2>Login</h2>
+
+//       <div className="flex flex-wrap justify-start gap-4">
+//         <div className="mb-4">
+//           <label
+//             className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+//             htmlFor="email"
+//           >
+//             Email
+//           </label>
+//           <input
+//             onChange={(e) => handleInputChange("email", e)}
+//             value={enteredValue.email}
+//             className="block w-full max-w-[15rem] p-2 text-base border border-solid border-[#758a8a] bg-[#869999] text-[#142020]"
+//             id="email"
+//             type="email"
+//             name="email"
+//           />
+//         </div>
+
+//         <div className="mb-4">
+//           <label
+//             className="block text-sm mb-1 text-[#9bafaf] uppercase font-bold"
+//             htmlFor="password"
+//           >
+//             Password
+//           </label>
+//           <input
+//             onChange={(e) => handleInputChange("password", e)}
+//             value={enteredValue.password}
+//             className="block w-full max-w-[15rem] p-2 text-base border border-solid border-[#758a8a] bg-[#869999] text-[#142020]"
+//             id="password"
+//             type="password"
+//             name="password"
+//           />
+//         </div>
+//       </div>
+//     </form>
+//   );
+// }
